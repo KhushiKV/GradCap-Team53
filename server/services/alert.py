62ca0001aa998b0b9dfbf74fdcf34b54b98651c9
team53-interiit-11 <122287288+team53-interiit-11@users.noreply.github.com>
2023-02-17 00:57:57 +0530

    Update alert.py

--- server/services/alert.py
+++ server/services/alert.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 alert_ref = db.collection('alerts')
 
-client = Client("AC621f85babe090a21bc31c54d9bd74474",
-                "a4d73d1f589c9117223c514a51ff1d06")
+client = Client("ssid",
+                "auth_token")
 
 
 def create_alert():
@@ -14,7 +14,7 @@ def create_alert():
     link = 'https://www.google.com/maps/search/?api=1&query=' + \
         str(location[0]) + '%2c' + str(location[1]) + ''
 
-    client.messages.create(to=["+916393716756"], from_="+16064044591",
+    client.messages.create(to=[""], from_="+16064044591",
                            body="This is the location of emergency: " +
                            link + " (Shared via SMS)")
 
@@ -22,7 +22,7 @@ def create_alert():
 
     call = client.calls.create(
         twiml='<Response><Say voice="alice">An emergency has been detected! Location has been shared via SMS.</Say></Response>',
-        url='https://handler.twilio.com/twiml/EHa25f9617d549a5c9b6ae1f41dbd27205', to='+916393716756', from_='+16064044591')
+        url='https://handler.twilio.com/twiml/EHa25f9617d549a5c9b6ae1f41dbd27205', to='', from_='+16064044591')
     print(call.sid)
 
     alert_ref.add(request.json)
